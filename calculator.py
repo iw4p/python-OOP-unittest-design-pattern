43b54495a87cf11581548b91b4ceef49bdd2af5a
Nima Akbarzade <Nimakbarzade@yahoo.com>
2022-07-26 17:24:42 +0430

    init

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculation():
+    def __init__(self, first_number, second_number, operation):
+        self.first_number = first_number
+        self.second_number = second_number
+        self.operation = operation
+
+    def __str__(self):
+        return 
