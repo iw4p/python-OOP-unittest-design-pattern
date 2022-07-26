cb283ce42a217c3b56571a6fd5c872e0df88c43b
Nima Akbarzade <Nimakbarzade@yahoo.com>
2022-07-26 19:08:15 +0430

    fix all unittests - nose2 runner

--- test_calculator.py
+++ test_calculator.py
@@ -1,20 +1,23 @@
 from calculator import Calculator
 
 def test_sum():
+    calculator_object = Calculator()
     assert calculator_object.sum(5, 5) == 10, "Should be 10"
 
 def test_sub():
+    calculator_object = Calculator()
     assert calculator_object.sub(5, 5) == 0, "Should be 0"
 
 def test_mul():
+    calculator_object = Calculator()
     assert calculator_object.mul(5, 5) == 25, "Should be 25"
 
 def test_div():
+    calculator_object = Calculator()
     assert calculator_object.div(5, 5) == 1, "Should be 11"
 
 if __name__ == "__main__":
 
-    calculator_object = Calculator()
     # calculator_object.first_number = 5
     # calculator_object.second_number = 5
 
